aed6a14fec89c229e50ce5100bd3ff34c6c6f386
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-03-29 21:24:55 +0200

    simplify build.js

--- build.js
+++ build.js
@@ -1,7 +1,3 @@
 import sass from "sass";
-import { promises as fs } from "fs";
 
-(async () => {
-  const { css } = await sass.compile("input.scss");
-  await fs.writeFile("output.css", css);
-})();
+sass.compile("input.scss", {});
